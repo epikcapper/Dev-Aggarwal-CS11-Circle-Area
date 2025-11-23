b6033c3b058434a63f5574ea92d687b2a59ecdad
Dev Aggarwal <96664051+epikcapper@users.noreply.github.com>
2025-11-22 16:50:56 -0800

    Add files via upload

---
+++ circle_area.py
@@ -0,0 +1,6 @@
+import math
+
+print("This is the circle area calculator!")
+radius = float(input("What is the radius of the circle? "))
+area = math.pi * radius ** 2
+print("The area of your circle is:", round(area, 2))
